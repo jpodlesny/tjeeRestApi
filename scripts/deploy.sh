04a1a5d1bd8c9a06d82424adbcade6e479f101c7
jpodlesny <jpodlesny@sigma.ug.edu.pl>
2017-01-23 22:54:39 +0100

    Poprawki

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo "************ UNDEPLOYING *******************"
-/opt/devel/glassfish4/bin/asadmin undeploy szpitalRestApi
+/opt/devel/glassfish4/bin/asadmin undeploy szpitalRestApi-1
 echo "************ BUILDING **********************"
 cd ..
 mvn clean
 mvn package
 echo "************ DEPLOYING *********************"
-/opt/devel/glassfish4/bin/asadmin deploy target/szpitalRestApi.war
+/opt/devel/glassfish4/bin/asadmin deploy target/szpitalRestApi-1.war
